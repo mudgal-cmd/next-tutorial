c58d4affa82abef793fd8b243a8e95443e346d11
mudgal-cmd <nishant2020.mudgal@gmail.com>
2024-09-04 22:42:06 -0500

    Created a next app and posts route + footer + header

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+export default function Posts() {
+  return(
+    <main className="text-center pt-32 px-5">
+
+      <h1 className="text-4xl md:text-5xl font-bold">All Posts</h1>
+    
+    </main>
+  )
+}
